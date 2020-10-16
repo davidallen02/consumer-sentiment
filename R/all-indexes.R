4953165e5dc3dcdea96ea930bb412db7003133e7
David Allen <davidallen02@me.com>
2020-10-16 15:14:06 -0700

    bulk

--- R/all-indexes.R
+++ R/all-indexes.R
@@ -1,6 +1,17 @@
 
 library(magrittr)
 
+dat <- pamngr::join_sheets(c("conssent", "conscurr","consexp")) %>%
+  dplyr::slice_max(dates, n = 60) %>%
+  magrittr::set_colnames(c("dates", "Headline", "Current Conditions", "Expectations")) %>%
+  tidyr::pivot_longer(cols = -dates, names_to = "variable")
+
+p <- dat %>%
+  pamngr::lineplot() %>%
+  pamngr::pam_plot(plot_title = "Consumer Sentiment")
+
+p %>% pamngr::all_output("consumer-sentiment")
+
 df1 <- pamngr::get_data("conssent")
 df2 <- pamngr::get_data("conscurr")
 df3 <- pamngr::get_data("consexp")
